887c667d075705085d18abcedcaf6f8e118d580a
mu7kata <mu7kata@icloud.com>
2020-11-23 23:52:08 +0900

    認証機能追加

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,11 @@ $movie->delete();
 return redirect('/');
 
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
